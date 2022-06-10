dd5943300a008fdacb1724ddf3484116e175cc83
Prateekraj1 <67007560+Prateekraj1@users.noreply.github.com>
2022-06-10 20:42:45 +0530

    Delete 66314755.py

--- 66314755.py
+++
@@ -1,9 +0,0 @@
-# cook your dish here
-itera=int(input())
-for i in range(itera):
-    x,y,a=input().split(" ")
-    x=int(x);y=int(y);a=int(a)
-    if(a>=x and a<y):
-        print("YES")
-    else:
-        print("NO")
