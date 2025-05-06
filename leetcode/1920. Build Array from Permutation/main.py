aa820f6c95b05373995d28d6728a89c2a09c1a85
Prateek Raj <67007560+Prateekraj1@users.noreply.github.com>
2025-05-06 22:56:10 +0530

    leetcode 1920 problem solved

---
+++ leetcode/1920. Build Array from Permutation/main.py
@@ -0,0 +1,7 @@
+#1920. Build Array from Permutation solution
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in nums:
+            ans.append(nums[i])
+        return ans
